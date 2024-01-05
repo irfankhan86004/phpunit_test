c40b133b4f519d0ac2caaf52bdcb8b850350b9d2
Lucky Boy <iamirfanwebdeveloper@gmail.com>
2024-01-05 16:33:48 +0530

    all code testing

--- tests/AccountTest.php
+++ tests/AccountTest.php
@@ -7,5 +7,7 @@ class AccountTest extends TestCase
     public function testGetAccounts()
     {
         $this->assertEquals(1, 1);
+		
+		//https://about.codecov.io/blog/measuring-php-code-coverage-with-phpunit-and-github-actions/
     }
 }
