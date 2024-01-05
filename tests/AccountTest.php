3836c45b8c67a477259572de73e9499626b54c99
Lucky Boy <iamirfanwebdeveloper@gmail.com>
2024-01-05 16:52:57 +0530

    all code testing

--- tests/AccountTest.php
+++ tests/AccountTest.php
@@ -4,10 +4,11 @@ use PHPUnit\Framework\TestCase;
 
 class AccountTest extends TestCase
 {
-    public function testAccounts(): void 
+    public function testAccounts()
     {
         $this->assertEquals(1, 1);
 		
+		//https://about.codecov.io/blog/measuring-php-code-coverage-with-phpunit-and-github-actions/
 		//https://about.codecov.io/blog/measuring-php-code-coverage-with-phpunit-and-github-actions/
     }
 }
